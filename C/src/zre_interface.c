b616c076ff7e8abb97e20bb1f2ba56bed2adc1fb
KIU Shueng Chuan <nixchuan@gmail.com>
2012-12-15 23:09:05 +0800

    add uuid and mkdir support for Windows platform

--- C/src/zre_interface.c
+++ C/src/zre_interface.c
@@ -25,7 +25,9 @@
 */
 
 #include <czmq.h>
+#if (!defined (__WINDOWS__))
 #include <uuid/uuid.h>
+#endif
 #include "../include/zre.h"
 
 
@@ -199,15 +201,32 @@ zre_interface_publish (zre_interface_t *self, char *pathname, char *virtual)
 #define BEACON_PROTOCOL     "ZRE"
 #define BEACON_VERSION      0x01
 
+#pragma pack(1)
 typedef struct {
     byte protocol [3];
     byte version;
     uuid_t uuid;
     uint16_t port;
 } beacon_t;
+#pragma pack()
 
 //  Convert binary UUID to freshly allocated string
 
+#if (defined (__WINDOWS__))
+static char *
+s_uuid_str (uuid_t *uuid)
+{
+    char hex_char [] = "0123456789ABCDEF";
+    char *string = zmalloc (sizeof (uuid_t) * 2 + 1);
+    int byte_nbr;
+    for (byte_nbr = 0; byte_nbr < sizeof (uuid_t); byte_nbr++) {
+	int val = ((unsigned char *) uuid) [byte_nbr];
+        string [byte_nbr * 2 + 0] = hex_char [val >> 4];
+        string [byte_nbr * 2 + 1] = hex_char [val & 15];
+    }
+    return string;
+}
+#else
 static char *
 s_uuid_str (uuid_t uuid)
 {
@@ -220,7 +239,7 @@ s_uuid_str (uuid_t uuid)
     }
     return string;
 }
-
+#endif
 
 //  This structure holds the context for our agent, so we can
 //  pass that around cleanly to methods which need it
@@ -270,8 +289,13 @@ agent_new (zctx_t *ctx, void *pipe)
         free (self);
         return NULL;
     }
+#if (defined (__WINDOWS__))
+    UuidCreate (&self->uuid);
+    self->identity = s_uuid_str (&self->uuid);
+#else
     uuid_generate (self->uuid);
     self->identity = s_uuid_str (self->uuid);
+#endif
     self->peers = zhash_new ();
     self->peer_groups = zhash_new ();
     self->own_groups = zhash_new ();
@@ -285,13 +309,23 @@ agent_new (zctx_t *ctx, void *pipe)
     //
 #   define OUTBOX   ".outbox"
     sprintf (self->fmq_outbox, "%s/%s", OUTBOX, self->identity);
+#if (defined (__WINDOWS__))
+    CreateDirectory (OUTBOX, NULL);
+    CreateDirectory (self->fmq_outbox, NULL);
+#else
     mkdir (OUTBOX, 0775);
     mkdir (self->fmq_outbox, 0775);
+#endif
     
 #   define INBOX    ".inbox"
     sprintf (self->fmq_inbox, "%s/%s", INBOX, self->identity);
+#if (defined (__WINDOWS__))
+    CreateDirectory (INBOX, NULL);
+    CreateDirectory (self->fmq_inbox, NULL);
+#else
     mkdir (INBOX, 0775);
     mkdir (self->fmq_inbox, 0775);
+#endif
 
     self->fmq_server = fmq_server_new ();
     self->fmq_service = fmq_server_bind (self->fmq_server, "tcp://*:*");
@@ -652,7 +686,11 @@ agent_beacon_send (agent_t *self)
     beacon.protocol [1] = 'R';
     beacon.protocol [2] = 'E';
     beacon.version = BEACON_VERSION;
+#if (defined (__WINDOWS__))
+    memcpy (&beacon.uuid, &self->uuid, sizeof (uuid_t));
+#else
     memcpy (beacon.uuid, self->uuid, sizeof (uuid_t));
+#endif
     beacon.port = htons (self->port);
     
     //  Broadcast the beacon to anyone who is listening
@@ -678,8 +716,13 @@ agent_recv_udp_beacon (agent_t *self)
         return 0;               //  Ignore invalid beacons
 
     //  If we got a UUID and it's not our own beacon, we have a peer
+#if (defined (__WINDOWS__))
+    if (memcmp (&beacon.uuid, &self->uuid, sizeof (uuid_t))) {
+        char *identity = s_uuid_str (&beacon.uuid);
+#else
     if (memcmp (beacon.uuid, self->uuid, sizeof (uuid_t))) {
         char *identity = s_uuid_str (beacon.uuid);
+#endif
         zre_peer_t *peer = s_require_peer (
             self, identity, zre_udp_from (self->udp), ntohs (beacon.port));
         zre_peer_refresh (peer);
