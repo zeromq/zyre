a1c11d0f3fe64c6d3df7d63a7b6bd72429cce486
Maarten Ditzel <maarten.ditzel@tno.nl>
2014-04-10 00:03:35 +0200

    Added cmake support (using makefile generation similar to czmq).

---
+++ FindCZMQ.cmake
@@ -0,0 +1,18 @@
+include(FindPkgConfig)
+PKG_CHECK_MODULES(PC_CZMQ "libczmq")
+
+find_path(
+    CZMQ_INCLUDE_DIRS
+    NAMES czmq.h
+    HINTS ${PC_CZMQ_INCLUDE_DIRS}
+)
+
+find_library(
+    CZMQ_LIBRARIES
+    NAMES czmq
+    HINTS ${PC_CZMQ_LIBRARY_DIRS}
+)
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CZMQ DEFAULT_MSG CZMQ_LIBRARIES CZMQ_INCLUDE_DIRS)
+mark_as_advanced(CZMQ_LIBRARIES CZMQ_INCLUDE_DIRS)
