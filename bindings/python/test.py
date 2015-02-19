22d27709fa6d47726a82e5cec61dd21b8fa7559b
Tom Whittock <tom.whittock@gmail.com>
2015-02-19 22:08:27 +0800

    Problem: no python bindings for zyre.
    Solution: Regenerate using latest zproject

---
+++ bindings/python/test.py
@@ -0,0 +1,49 @@
+import unittest
+from zyre import Zyre, ZyreEvent
+
+class TestZyre(unittest.TestCase):
+    def test_all(self):
+        z1 = Zyre('t1')
+        z2 = Zyre('t2')
+
+        z1.set_header('test_header', 'test1')
+        z2.set_header('test_header', 'test2')
+
+        self.assertEquals(z1.start(), 0)
+        self.assertEquals(z2.start(), 0)
+
+        e = ZyreEvent(z1)
+        self.assertEquals(e.name(), z2.name())
+        self.assertEquals(e.sender(), z2.uuid())
+        self.assertEquals(e.type(), 'enter')
+
+        e = ZyreEvent(z2)
+        self.assertEquals(e.name(), z1.name())
+        self.assertEquals(e.sender(), z1.uuid())
+        self.assertEquals(e.type(), 'enter')
+
+        z1.join('test group')
+        z2.join('test group')
+
+        e = ZyreEvent(z1)
+        self.assertEquals(e.name(), z2.name())
+        self.assertEquals(e.sender(), z2.uuid())
+        self.assertEquals(e.type(), 'join')
+        self.assertEquals(e.group(), 'test group')
+
+        e = ZyreEvent(z2)
+        self.assertEquals(e.name(), z1.name())
+        self.assertEquals(e.sender(), z1.uuid())
+        self.assertEquals(e.type(), 'join')
+        self.assertEquals(e.group(), 'test group')
+
+        z1.shouts('test group', "Hello World")
+
+        e = ZyreEvent(z2)
+        self.assertEquals(e.name(), z1.name())
+        self.assertEquals(e.sender(), z1.uuid())
+        self.assertEquals(e.type(), 'shout')
+        self.assertEquals(e.group(), 'test group')
+
+if __name__ == '__main__':
+    unittest.main()
