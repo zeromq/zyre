c1abddb99559fdc312d3843af5e4386bf16872c7
AdamWynne <awynne@gmail.com>
2015-09-18 09:26:48 -0400

    added test for SHOUTs

--- bindings/java/zyre-jni/src/test/java/org/zyre/jni/test/ShoutWhisperTest.java
+++ bindings/java/zyre-jni/src/test/java/org/zyre/jni/test/ShoutWhisperTest.java
@@ -2,26 +2,29 @@ package org.zyre.jni.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zyre.Utils;
 import org.zyre.Zyre;
 
-public class PingPongWhisperTest {
+public class ShoutWhisperTest {
 	
-	private static final Logger log = LoggerFactory.getLogger(PingPongWhisperTest.class);
+	private static final Logger log = LoggerFactory.getLogger(ShoutWhisperTest.class);
 
 	public static final String PING = "ping";
 	public static final String PONG = "pong";
 	public static final String GROUP = "global";
 	
+	public static final int NUM_RESP = 3;
+	public static final int NUM_MSGS = 2;
+	
 	private Requester reqThread;
-	private Responder respThread;
+	private List<Responder> respThreads = new ArrayList<Responder>();
 	
 	private boolean passed = true;
 
@@ -29,27 +32,35 @@ public class PingPongWhisperTest {
 	public void test() throws Exception {
 		
 		reqThread = new Requester();
-		respThread = new Responder();
 		
 		// start requester, which waits for the responder to JOIN
 		reqThread.init();
 		reqThread.start(); 
 
-		// start responder
-		respThread.init();
-		respThread.start();
+		// start responders
+		for (int i=0; i < NUM_RESP; i++) {
+			Responder resp = new Responder();
+			resp.init();
+			resp.start();
+			respThreads.add(resp);
+		}
 
-		// wait for responder to finish
-		respThread.join();
-		respThread.destroy();
+		// wait for responders to finish
+		for (Responder resp : respThreads) {
+			resp.join();
+		}
 
 		// wait for requester to finish
 		reqThread.join();
 		reqThread.destroy();
 
+		// destroy all responders once requester is done
+		for (Responder resp : respThreads) {
+			resp.destroy();
+		}
+
 		// leave some time for resources to be freed
-		try { Thread.sleep(100); } 
-		catch (InterruptedException e) { e.printStackTrace(); }
+		Thread.sleep(100);
 		
 		assertTrue(passed);
 	}
@@ -61,7 +72,9 @@ public class PingPongWhisperTest {
 		
 		public void run() {
 			String peer;
-			// wait for responder to join
+			int joinCt = 0;
+
+			// wait for responders to join
 			while (true) {
 				String msg = zyre.recv();
 				HashMap<String,String> map = Utils.parseMsg(msg);
@@ -74,13 +87,27 @@ public class PingPongWhisperTest {
 						passed = false;
 						return;
 					}
+					joinCt++;
 					log.info("responder joined: " + peer);
-					break;
+
+					if (joinCt == NUM_RESP) {
+						log.info("All responders joined: " + NUM_RESP);
+						break;
+					}
 				}
 			}
-			log.info("sending ping");
-			zyre.whisper(peer, PING);
+
+			try { Thread.sleep(200); } 
+			catch (InterruptedException e) { e.printStackTrace(); }
+
+			log.info("sending ping(s) via SHOUT: " + NUM_MSGS);
+			for (int i=0; i < NUM_MSGS; i++) {
+				zyre.shout(GROUP, PING);
+			}
 			
+			int expected = NUM_RESP * NUM_MSGS;
+			int recvCt = 0;
+
 			while(true) {
 				String msg = zyre.recv();
 				HashMap<String,String> map = Utils.parseMsg(msg);
@@ -88,32 +115,41 @@ public class PingPongWhisperTest {
 				
 				if (event.equals("WHISPER")) {
 					log.info("requester received response: " + msg);
+
 					String text = map.get("message");
 					if (!text.equals(PONG)) {
 						log.error("Did not receive PONG.  Message was: " + text);
 						passed = false;
 					}
-					break;
+					recvCt++;
+					if (recvCt == expected) {
+						log.info("received all messages: " + expected);
+						break;
+					}
+					else {
+						log.info("received: " + recvCt + " expected: " + expected);
+					}
+				}
+				else {
+					log.info("ignoring event: " + msg);
 				}
 			}
-			
 		}
-
 	}
 	
 	private class Responder extends ZyreThread {
 		
 		public void run() {
+			int sentCt = 0;
 
-			log.info("responder running");
 			while(!Thread.currentThread().isInterrupted()) {
 				String msg = zyre.recv();
 				HashMap<String,String> map = Utils.parseMsg(msg);
 				
 				String event = map.get("event");
 				
-				if (event.equals("WHISPER")){
-					log.info("responder received: " + msg);
+				if (event.equals("SHOUT")){
+					log.info("responder received shout: " + msg);
 					String text = map.get("message");
 					String peer = map.get("peer");
 					
@@ -124,7 +160,11 @@ public class PingPongWhisperTest {
 				
 					log.info("sending pong");
 					zyre.whisper(peer, PONG);
-					break;
+					sentCt++;
+					
+					if (sentCt == NUM_MSGS) {
+						break;
+					}
 				}
 			}
 		}
