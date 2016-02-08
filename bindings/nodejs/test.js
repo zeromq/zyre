f5e48c5d8bbef90858e936308faaf57130c0fa2d
Pieter Hintjens <ph@imatix.com>
2016-02-08 16:47:27 +0100

    Problem: test.js exits immediately
    Solution: press any key to exit

--- bindings/nodejs/test.js
+++ bindings/nodejs/test.js
@@ -2,5 +2,12 @@
 
 var ZyreBinding = require ('bindings')('zyre');
 var zyre = new ZyreBinding.Zyre ();
+
 console.log ('Node name is: ' + zyre.name ());
+
+console.log ('Press any key to exit');
+process.stdin.setRawMode (true);
+process.stdin.resume ();
+process.stdin.on ('data', process.exit.bind (process, 0));
+
 zyre.destroy ();
