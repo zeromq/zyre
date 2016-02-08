3944ec64c3acf278ac9cb06f91f7a6c1858b8048
Pieter Hintjens <ph@imatix.com>
2016-02-08 17:54:27 +0100

    Problem: test.js exits immediately
    Minimal solution is just to sleep for 2 seconds. This gives
    you time to see the node enter and exit on a second terminal
    running zpinger.

--- bindings/nodejs/test.js
+++ bindings/nodejs/test.js
@@ -4,10 +4,9 @@ var ZyreBinding = require ('bindings')('zyre');
 var zyre = new ZyreBinding.Zyre ();
 
 console.log ('Node name is: ' + zyre.name ());
+console.log ('Waiting for 2 seconds, then exiting');
 
-console.log ('Press any key to exit');
-process.stdin.setRawMode (true);
-process.stdin.resume ();
-process.stdin.on ('data', process.exit.bind (process, 0));
+var sleep = require ('sleep');
+sleep.sleep (2);
 
 zyre.destroy ();
