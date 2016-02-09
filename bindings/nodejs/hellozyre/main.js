ad8c6664832480930f1149d6970a09c34bf49ef3
Benjamin Henrion <zoobab@gmail.com>
2016-02-09 16:33:32 +0100

    add hellozyre example

---
+++ bindings/nodejs/hellozyre/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const electron = require ('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+var zyre = null;
+
+var ipcMain = require('electron').ipcMain;
+
+global.sharedObj = {prop1: "hello3"};
+
+ipcMain.on('show-prop1', function(event) {
+  console.log(global.sharedObj.prop1);
+});
+
+
+console.log(process.versions)
+
+app.on ('window-all-closed', function () {
+    if (process.platform != 'darwin') {
+        app.quit ();
+    }
+});
+
+app.on ('ready', function () {
+    var ZyreBinding = require ('bindings')('zyre');
+    var zyre = new ZyreBinding.Zyre ();
+    var zyrename = zyre.name ();
+    console.log ('Node name is: ' + zyrename + ' EOL');
+    global.sharedObj = {prop1: zyrename};
+
+    mainWindow = new BrowserWindow ({ width:800, height:600 });
+    mainWindow.loadURL ('file://' + __dirname + '/index.html');
+    mainWindow.webContents.openDevTools ();
+
+    mainWindow.on ('closed', function () {
+        zyre.destroy ();
+        mainWindow = null;
+    });
+});
