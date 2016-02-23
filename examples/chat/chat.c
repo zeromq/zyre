6026323a0e0d8d4a8878f497bf6ffd1964d452cd
Pieter Hintjens <ph@imatix.com>
2016-02-23 13:23:20 +0100

    Problem: chat example has poor formatting
    Solution: fix it.

--- examples/chat/chat.c
+++ examples/chat/chat.c
@@ -1,8 +1,7 @@
 //  --------------------------------------------------------------------------
 //  Example Zyre distributed chat application
 //
-//  --------------------------------------------------------------------------
-//  Copyright (c) 2010-2014 iMatix Corporation and Contributors
+//  Copyright (c) 2010-2014 The Authors
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a
 //  copy of this software and associated documentation files (the "Software"),
@@ -32,12 +31,10 @@
 static void 
 chat_actor (zsock_t *pipe, void *args)
 {
-    //  Do some initialization
-    char*   name = (char*) args;
-    zyre_t *node = zyre_new (name);
+    zyre_t *node = zyre_new ((char *) args);
     if (!node)
         return;                 //  Could not create new node
-    //zyre_set_verbose (node);  // uncomment to watch the events
+
     zyre_start (node);
     zyre_join (node, "CHAT");
     zsock_signal (pipe, 0);     //  Signal "ready" to caller
@@ -45,20 +42,20 @@ chat_actor (zsock_t *pipe, void *args)
     bool terminated = false;
     zpoller_t *poller = zpoller_new (pipe, zyre_socket (node), NULL);
     while (!terminated) {
-        void *which = zpoller_wait (poller, -1); // no timeout
-        if (which == pipe){
+        void *which = zpoller_wait (poller, -1);
+        if (which == pipe) {
             zmsg_t *msg = zmsg_recv (which);
             if (!msg)
                 break;              //  Interrupted
+
             char *command = zmsg_popstr (msg);
-            if (streq (command, "$TERM")) {
+            if (streq (command, "$TERM"))
                 terminated = true;
-            }
             else
             if (streq (command, "SHOUT")) { 
                 char *string = zmsg_popstr (msg);
                 zyre_shouts (node, "CHAT", "%s", string);
-	    }
+            }
             else {
                 puts ("E: invalid message to actor");
                 assert (false);
@@ -66,7 +63,8 @@ chat_actor (zsock_t *pipe, void *args)
             free (command);
             zmsg_destroy (&msg);
         }
-        else if (which == zyre_socket (node)) {
+        else
+        if (which == zyre_socket (node)) {
             zmsg_t *msg = zmsg_recv (which);
             char *event = zmsg_popstr (msg);
             char *peer = zmsg_popstr (msg);
@@ -74,20 +72,17 @@ chat_actor (zsock_t *pipe, void *args)
             char *group = zmsg_popstr (msg);
             char *message = zmsg_popstr (msg);
 
-            if (streq (event, "ENTER")) { 
+            if (streq (event, "ENTER"))
                 printf ("%s has joined the chat\n", name);
-            }
-            else if (streq (event, "EXIT")) { 
+            else
+            if (streq (event, "EXIT"))
                 printf ("%s has left the chat\n", name);
-            }
-            else if (streq (event, "SHOUT")) { 
+            else
+            if (streq (event, "SHOUT"))
                 printf ("%s: %s\n", name, message);
-            }
-	    else if (streq (event, "EVASIVE")) {
-	    	printf ("%s is being evasive\n", name);
-	    }
-            //printf ("Message from node\n");
-            //printf ("event: %s peer: %s  name: %s\n  group: %s message: %s\n", event, peer, name, group, message);
+            else
+            if (streq (event, "EVASIVE"))
+                printf ("%s is being evasive\n", name);
 
             free (event);
             free (peer);
@@ -98,38 +93,28 @@ chat_actor (zsock_t *pipe, void *args)
         }
     }
     zpoller_destroy (&poller);
-
-    // Notify peers that this peer is shutting down. Provide
-    // a brief interval to ensure message is emitted.
-    zyre_stop(node);
-    zclock_sleep(100);
-
+    zyre_stop (node);
+    zclock_sleep (100);
     zyre_destroy (&node);
 }
 
-
-
 int
-main (int argc, char *argv[])
+main (int argc, char *argv [])
 {
     if (argc < 2) {
         puts ("syntax: ./chat myname");
         exit (0);
     }
-    zactor_t *actor = zactor_new (chat_actor, argv[1]);
+    zactor_t *actor = zactor_new (chat_actor, argv [1]);
     assert (actor);
     
     while (!zsys_interrupted) {
         char message [1024];
-        if (!fgets( message, 1024, stdin))
-	        break;
-	    message[strlen(message)-1] = 0; // drop the trailing linefeed
-	    zstr_sendx (actor, "SHOUT", message, NULL);
+        if (!fgets (message, 1024, stdin))
+            break;
+        message [strlen (message) - 1] = 0;     // Drop the trailing linefeed
+        zstr_sendx (actor, "SHOUT", message, NULL);
     }
-
     zactor_destroy (&actor);
-
     return 0;
 }
-
-
