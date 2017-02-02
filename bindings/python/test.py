45bed24dd400224795ca371d6f3b118b2d7b8014
Hugo Herter <git@hugoherter.com>
2017-02-02 16:24:58 +0100

    Problem: ctypes c_char_b maps to bytes in Python3 Solution: prepend all strings with 'b' so Python3 interprets them as bytes, while is is transparent to Python2

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -3,11 +3,11 @@ from zyre import Zyre, ZyreEvent
 
 class TestZyre(unittest.TestCase):
     def test_all(self):
-        z1 = Zyre('t1')
-        z2 = Zyre('t2')
+        z1 = Zyre(b't1')
+        z2 = Zyre(b't2')
 
-        z1.set_header('test_header', 'test1')
-        z2.set_header('test_header', 'test2')
+        z1.set_header(b'test_header', b'test1')
+        z2.set_header(b'test_header', b'test2')
 
         self.assertEquals(z1.start(), 0)
         self.assertEquals(z2.start(), 0)
@@ -15,36 +15,36 @@ class TestZyre(unittest.TestCase):
         e = ZyreEvent(z1)
         self.assertEquals(e.peer_name(), z2.name())
         self.assertEquals(e.peer_uuid(), z2.uuid())
-        self.assertEquals(e.type(), 'ENTER')
+        self.assertEquals(e.type(), b'ENTER')
 
         e = ZyreEvent(z2)
         self.assertEquals(e.peer_name(), z1.name())
         self.assertEquals(e.peer_uuid(), z1.uuid())
-        self.assertEquals(e.type(), 'ENTER')
+        self.assertEquals(e.type(), b'ENTER')
 
-        z1.join('test group')
-        z2.join('test group')
+        z1.join(b'test group')
+        z2.join(b'test group')
 
         e = ZyreEvent(z1)
         self.assertEquals(e.peer_name(), z2.name())
         self.assertEquals(e.peer_uuid(), z2.uuid())
-        self.assertEquals(e.type(), 'JOIN')
-        self.assertEquals(e.group(), 'test group')
+        self.assertEquals(e.type(), b'JOIN')
+        self.assertEquals(e.group(), b'test group')
 
         e = ZyreEvent(z2)
         self.assertEquals(e.peer_name(), z1.name())
         self.assertEquals(e.peer_uuid(), z1.uuid())
-        self.assertEquals(e.type(), 'JOIN')
-        self.assertEquals(e.group(), 'test group')
+        self.assertEquals(e.type(), b'JOIN')
+        self.assertEquals(e.group(), b'test group')
 
-        z1.shouts('test group', "Hello World")
+        z1.shouts(b'test group', b"Hello World")
 
         e = ZyreEvent(z2)
         self.assertEquals(e.peer_name(), z1.name())
         self.assertEquals(e.peer_uuid(), z1.uuid())
-        self.assertEquals(e.type(), 'SHOUT')
-        self.assertEquals(e.group(), 'test group')
-        self.assertEquals(e.msg().popstr(), "Hello World")
+        self.assertEquals(e.type(), b'SHOUT')
+        self.assertEquals(e.group(), b'test group')
+        self.assertEquals(e.msg().popstr(), b"Hello World")
 
 if __name__ == '__main__':
     unittest.main()
