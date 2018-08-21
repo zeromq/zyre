f92266572d59e69abe594677195d9dbec2ed588b
Jim Klimov <jim@jimklimov.com>
2018-08-21 03:25:21 +0200

    check_zproject : revert to no tmp-deps/ version

--- builds/check_zproject/ci_build.sh
+++ builds/check_zproject/ci_build.sh
@@ -6,16 +6,14 @@ set -ex
 [ -n "${REPO_DIR-}" ] || exit 1
 
 # Verify all required dependencies with repos can be checked out
-rm -rf "$REPO_DIR/../tmp-deps"
-mkdir -p "$REPO_DIR/../tmp-deps"
-cd "$REPO_DIR/../tmp-deps"
+cd "$REPO_DIR/.."
 git clone --quiet --depth 1 https://github.com/zeromq/libzmq.git libzmq
 git clone --quiet --depth 1 https://github.com/zeromq/czmq.git czmq
 cd -
 
 if ! ((command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list zproject >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions zproject >/dev/null 2>&1)); then
-    cd "$REPO_DIR/../tmp-deps"
+    cd "$REPO_DIR/.."
     git clone --quiet --depth 1 https://github.com/zeromq/zproject zproject
     cd zproject
     PATH="`pwd`:$PATH"
@@ -23,7 +21,7 @@ fi
 
 if ! ((command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list generator-scripting-language >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions gsl >/dev/null 2>&1)); then
-    cd "$REPO_DIR/../tmp-deps"
+    cd "$REPO_DIR/.."
     git clone https://github.com/zeromq/gsl.git gsl
     cd gsl/src
     make
